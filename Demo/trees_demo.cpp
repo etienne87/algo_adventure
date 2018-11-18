78c98d25c34d7cadda4427791774ec51d202d7cd
etienne87 <et.perot@gmail.com>
2018-11-18 19:33:32 +0100

    adding a small tree & graph functions, dfs & bfs implemented using a stack

--- Demo/trees_demo.cpp
+++ Demo/trees_demo.cpp
@@ -13,10 +13,19 @@ int main(int argc, char* argv[]){
         exo = atoi(argv[1]);
 
     try{
-        
+        vector<Node> graph = build_binary_tree();
+        std::cout<<"bfs: "<<std::endl;
+        std::cout<<"==========="<<std::endl;
+        print_graph(&graph[0], false);
+        std::cout<<"dfs: "<<std::endl;
+        std::cout<<"==========="<<std::endl;
+        print_graph(&graph[0], true);
+
     }catch(std::exception& e){
         std::cout<<"Error: "<<e.what()<<std::endl;
     }
+
+    
 
 
 
--- Trees/trees.h
+++ Trees/trees.h
@@ -2,22 +2,84 @@
 #include <cstdlib> 
 #include <cstring>
 #include <unordered_set>
-#include <exception>
+#include <vector>
+#include <deque>
 
-template<class T>
+using std::vector;
+using std::pair;
+using std::unordered_set;
+using std::deque;
+
 struct Node
 {
-    Node(){
-        std::cout<<"Node ctor neutral called"<<std::endl;
-        next = NULL;
-    }
-    //interesting to remind: if you put here data = d, and you forget to make a operator =  for your T class
-    //then it will be deleted twice!
-    //here we call the copy ctor bc we made one, but it is also likely to crash if you forget to implement one
-    //Also we pass by reference, in order to avoid a copy
-    Node(const T& d):data(d){
-        next = NULL;
-    }
-    Node* next;
-    T data;
+    Node(int d=0):data(d){}
+    std::vector<Node*> children;
+    int data;
 };
+
+struct pair_hash {
+    inline std::size_t operator()(const std::pair<int,int> & v) const {
+        return v.first*31+v.second;
+    }
+};
+
+vector<Node> build_directed_graph(int size=10, int max_connect=3){
+    vector<Node> graph;
+    graph.resize(size);
+    unordered_set<pair<int,int>, pair_hash> visited;
+    for(int u=0;u<size;++u){
+        graph[u].data = rand()%100;
+        int connections = rand()%max_connect;
+        for(int j=0;j<connections;++j){
+            int v = rand()%size;
+            if(visited.find(pair<int,int>(v, u)) == visited.end()){
+                graph[u].children.push_back(&graph[v]);
+                visited.insert(pair<int,int>(u, v));
+            }
+        }
+    }
+    return graph;
+}
+
+vector<Node> build_binary_tree(int size=10){
+    vector<Node> graph;
+    graph.resize(size);
+    for(int i=0;i<size;i++){
+        graph[i].data = i;
+        if(2*i+1 < size)
+            graph[i].children.push_back(&graph[2*i+1]);
+        if(2*i+2 < size)
+            graph[i].children.push_back(&graph[2*i+2]);
+    }
+
+    return graph;
+}
+
+void print_graph(Node* node, bool dfs=true){
+    if(dfs){
+        deque<Node*> stack;
+        stack.push_front(node);
+        while(!stack.empty()){
+            Node* node = stack.front();
+            stack.pop_front();
+            std::cout<<node->data<<std::endl;
+            for(auto val: node->children){
+                stack.push_front(val);
+            }
+        }
+        
+    }else{
+        deque<Node*> queue;
+        queue.push_back(node);
+        while(!queue.empty()){
+            Node* node = queue.front();
+            queue.pop_front();
+            std::cout<<node->data<<std::endl;
+            for(auto val: node->children){
+                queue.push_back(val);
+            }
+        }
+        
+    }
+}
+
