28c7a12c44ccbe5cbd4c7221521ee597bdb47b43
etienne87 <et.perot@gmail.com>
2018-11-25 18:35:31 +0100

    adding new folder for bitwise manipulations

---
+++ Bitwise/bitwise.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib> 
+#include <cstring>
+#include <unordered_set>
+#include <unordered_map>
+#include <list>
+#include <vector>
+#include <deque>
+#include <string>
+#include <cmath>
+
+using std::vector;
---
+++ Demo/bitwise_demo.cpp
@@ -0,0 +1,26 @@
+#include <ctime> 
+#include <iostream>
+#include <vector>
+#include <time.h>
+#include <algorithm>
+#include "bitwise.h"
+
+
+int main(int argc, char* argv[]){
+    srand(0);
+    std::cout<<"Bitwise Demo"<<std::endl;
+    int exo = 0;
+    if(argc > 1)
+        exo = atoi(argv[1]);
+
+    try{
+      
+    }catch(std::exception& e){
+        std::cout<<"Error: "<<e.what()<<std::endl;
+    }
+
+    
+
+
+
+}
