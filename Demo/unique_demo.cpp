4111373139193a661970c8edc9c43671d0483ad3
etienne87 <et.perot@gmail.com>
2018-12-11 19:54:43 +0100

    added a sorted option

---
+++ Demo/unique_demo.cpp
@@ -0,0 +1,51 @@
+#include <ctime> 
+#include <iostream>
+#include <vector>
+#include <time.h>
+#include <algorithm>
+#include <random>
+#include "unique.h"
+#include "sort.h"
+
+using std::vector;
+
+int main(int argc, char* argv[]){
+    srand(0);
+    std::cout<<"Unique Demo"<<std::endl;
+    int size = 1e8;
+    int high = 640*480;
+    int rounds = 10;
+    bool sorted = false;
+    if(argc > 1)
+        size = atoi(argv[1]);
+    if(argc > 2)
+        high = atoi(argv[2]);    
+    if(argc > 3)
+        rounds = atoi(argv[3]);    
+    if(argc > 4)
+        sorted = bool(atoi(argv[4]));
+
+    vector<int> src(size);
+    fill_vector<int>(&src[0], src.size(), high);
+    vector<int> inv(size);
+    std::random_device rd;
+    std::mt19937 g(rd());
+
+    int nuniq = 0;
+    clock_t t = clock();
+    for(int i=0;i<rounds;i++){ 
+        if(sorted){
+            clock_t t2 = clock();
+            fill_vector<int>(&src[0], src.size(), high);
+            t -= clock()-t2;
+            //std::sort(src.begin(),src.end());  
+            radixsort(&src[0], src.size());        
+        }
+        unique_cpu<int>(&src[0], &inv[0], nuniq, size, sorted);
+    }
+    t = clock() - t;
+    float m_items = (float)size/1000000.0;
+    float runtime = ((float)t)/CLOCKS_PER_SEC/rounds;
+    float mevs = m_items/runtime;     
+    std::cout<<"Mev/s: "<<mevs<<" runtime: "<<runtime<<" for "<<m_items<<" Melems"<<std::endl;
+}
--- Unique/unique.h
+++ Unique/unique.h
@@ -13,20 +13,30 @@ void fill_vector(T* array, int size, T high=100){
 }
 
 template<class T>
-void unique_cpu(T* src, T* inv, int& num_uniques, int size){
+void unique_cpu(T* src, T* inv, int& num_uniques, int size, bool sorted=false){
     num_uniques = 0;
-    unordered_map<T,int> mmap;
-    for(int i=0;i<size;i++){
-        T val = src[i];
-        auto it = mmap.find(val);
-        int uniq_index = 0;
-        if( it == mmap.end()){
-            mmap.insert(pair<T,int>(val,num_uniques));
-            uniq_index = num_uniques;
-            num_uniques++;
-        }else{
-            uniq_index = it->second;
+    if(sorted){
+        inv[0] = 0;
+        for(int i=1;i<size-1;i++){
+            if(src[i-1] != src[i]){
+                num_uniques++;
+            }
+            inv[i] = num_uniques;
         }
-        inv[i] = uniq_index;
+    }else{
+        unordered_map<T,int> mmap;
+        for(int i=0;i<size;i++){
+            T val = src[i];
+            auto it = mmap.find(val);
+            int uniq_index = 0;
+            if( it == mmap.end()){
+                mmap.insert(pair<T,int>(val,num_uniques));
+                uniq_index = num_uniques;
+                num_uniques++;
+            }else{
+                uniq_index = it->second;
+            }
+            inv[i] = uniq_index;
+        }
     }
 }
