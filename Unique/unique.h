df9896af3adf72ec64c3f3fc9e7f4c1f6932e2f8
etienne87 <et.perot@gmail.com>
2018-12-11 19:39:59 +0100

    adding a simple unique function useful for sparse processing

---
+++ Unique/unique.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unordered_map>
+#include <utility>
+
+using std::unordered_map;
+using std::pair;
+
+template<class T>
+void fill_vector(T* array, int size, T high=100){
+    for(int i=0;i<size;++i){
+        array[i] = rand()%high;
+    }
+}
+
+template<class T>
+void unique_cpu(T* src, T* inv, int& num_uniques, int size){
+    num_uniques = 0;
+    unordered_map<T,int> mmap;
+    for(int i=0;i<size;i++){
+        T val = src[i];
+        auto it = mmap.find(val);
+        int uniq_index = 0;
+        if( it == mmap.end()){
+            mmap.insert(pair<T,int>(val,num_uniques));
+            uniq_index = num_uniques;
+            num_uniques++;
+        }else{
+            uniq_index = it->second;
+        }
+        inv[i] = uniq_index;
+    }
+}
