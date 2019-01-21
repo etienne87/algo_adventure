f1e33e50e0d47c27e90ec646c725c655e241aaac
etienne87 <et.perot@gmail.com>
2019-01-21 09:22:22 +0100

    missing 5.8 & 5.4

--- Bitwise/bitwise.h
+++ Bitwise/bitwise.h
@@ -17,3 +17,55 @@ int insert(int N, int M, int i, int j){
     int o = (~0 >> (32-j)) << i; // create 1 by moving by 32 
     return (N & ~o) | (M << i);
 }
+
+void print_binary(int N, int nbits=32){
+    int k = N;
+    for(int i=0;i<nbits;i++){
+        int bit = k & (1<<(nbits - 1 - i));
+        std::cout<<(bit>0);
+    }
+    std::cout<<std::endl;
+}
+
+int flip_to_win(int N){
+    vector<int> seq;
+    int c=0;
+    for(int i=0;i<32;i++){
+        int bit = (N & (1<<i))>0;
+        if(bit){
+            c++;
+        }else if(c > 0){
+            seq.push_back(c);
+            c = 0;
+        }
+    }
+    int max = 0;
+    for(int i=0;i<seq.size()-1;i++){
+        int c = seq[i] + seq[i+1] + 1;
+        if( c > max )
+            max = c;
+    }
+    return max;
+}
+
+int count_set_bits(int N){
+    int c=0;
+    for(int i=0;i<32;i++)
+        c += N & (1>>i);
+    return c;
+}
+
+int count_toflip_bits(int N){
+    int c=0;
+    for(int i=0;i<32;i++)
+        c += (N ^ (1<<i))==0;
+    return c;
+}
+
+int pairwise_swap(int N){
+    int mask_even = 0b10101010101010101010101010101010;
+    int mask_odd = mask_even>>1;
+    int odd_part = ((N & mask_odd)<<1)&mask_even;
+    int odd_even = ((N & mask_even)>>1)&mask_odd;
+    return odd_part | odd_even;
+}
--- Demo/bitwise_demo.cpp
+++ Demo/bitwise_demo.cpp
@@ -22,14 +22,24 @@ int main(int argc, char* argv[]){
     if(exo == 0){
         int N = 0b10111111101;
         int M = 0b0000010011;
-
-        
         int R =  insert(N, M, 2, 6);
         std::cout<<std::bitset<12>(R)<<std::endl;
-
+        print_binary(R, 12);
     }
-    
-
+    if(exo == 1){
+        int N = 0b11011101111;
+        int c = flip_to_win(N);
+        std::cout<<c<<std::endl;
+    }
+    if(exo == 2){
+        int N = 0b1110000;
+        print_binary(N-1);
+    }
+    if(exo == 3){
+        int N = 0b0101;
+        int R = pairwise_swap(N);
+        print_binary(R, 4);
+    }
 
 
 }
