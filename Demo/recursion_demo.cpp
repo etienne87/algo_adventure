46744b01e7c9d2774a1044dcdff85e8577d85fa7
etienne87 <et.perot@gmail.com>
2019-02-03 12:13:15 +0100

    adding chapter on dynamic programming

---
+++ Demo/recursion_demo.cpp
@@ -0,0 +1,18 @@
+#include <ctime> 
+#include <iostream>
+#include <vector>
+#include <time.h>
+#include <algorithm>
+#include "recursion.h"
+
+
+int main(int argc, char* argv[]){
+    srand(0);
+    std::cout<<"Recursion Demo"<<std::endl;
+    int exo = 0;
+    if(argc > 1)
+        exo = atoi(argv[1]);
+
+
+
+}
